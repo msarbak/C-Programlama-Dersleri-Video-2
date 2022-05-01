476d266c79753b1d0b649c3fce4907eb5bd4f933
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-05-02 00:32:33 +0300

    first commit

---
+++ program.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+#include "conio.h"
+
+typedef enum{false, true}bool;
+
+int main(){
+	char c = '\b';
+	//int a = 5;
+	//printf("Merhaba%c%c%c%c%c%c",c,c,c,c,c,c);
+	getch();
+	
+	if(0) printf("calisiyor\n");
+	if(true) printf("calisiyor2\n");
+	
+	
+	
+	
+	bool kontrol;
+	kontrol = false;
+	if(kontrol){ 
+	printf("calisti");
+	}
+	else {
+		printf("\ndfsf");
+		}
+	
+	return 0;
+}
